b762d84d68125d18b739cf4c99627fecc8338ab6
Noah Hefner <noah.hefner127@gmail.com>
2019-04-28 12:53:46 -0400

    moved a file, making test classes

---
+++ Classes/PlayerTest.py
@@ -0,0 +1,13 @@
+import unittest
+import Player
+
+
+class PlayerTest(unittest.TestCase):
+
+    player = Player("player_blue.png")
+
+    def test_change_speed(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
